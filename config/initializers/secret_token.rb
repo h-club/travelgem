e63befaf0a79cab9b33d4cbb3e10152939b4f61e
hclub <ec2-user@ip-172-31-23-33.us-west-2.compute.internal>
2016-03-11 15:38:23 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Travelgem::Application.config.secret_key_base = 'c9aa8973500eb782bb5b3c60a7ec0c9ee1de5ee3344a3f901bafd6ebf0fb6132f29dba096b7c2a352dc446fccc661314c23faf54ce61a5de1c13a276ca89410e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Travelgem::Application.config.session_store :cookie_store, key: '_travelgem_session'
