aaddccf1aa9f57740be73add6eb16492906d1b39
hclub <ec2-user@ip-172-31-23-33.us-west-2.compute.internal>
2016-03-11 15:51:33 +0000

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Travelgem::Application.routes.draw do
+	root 'home#hello'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
